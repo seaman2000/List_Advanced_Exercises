41ed79f80bba9a7dfe7254918185cc6398598227
Georgi Angelov <gtangelov76@gmail.com>
2026-02-06 14:37:07 +0200

    Update office_chairs.py

--- office_chairs.py
+++ office_chairs.py
@@ -5,12 +5,12 @@ chair_count = 0
 
 for room in range(1, number_of_rooms + 1):
     chairs, people = input().split()
-    room_number += 1
-    difference = len(chairs) - int(people)
+    people = int(people)
+    difference = len(chairs) - people
     chair_count += difference
 
-    if len(chairs) < int(people):
-        print(f"{abs(difference)} more chairs needed in room {room_number}")
+    if len(chairs) < people:
+        print(f"{abs(difference)} more chairs needed in room {room}")
 
 if chair_count >= 0:
     print(f"Game On, {chair_count} free chairs left")
