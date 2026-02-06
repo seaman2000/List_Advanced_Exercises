25478359bd23c9aa582e23b6a6daee051ed57ad7
Georgi Angelov <gtangelov76@gmail.com>
2026-02-07 01:14:08 +0200

    Update pokemon_dont_go.py

--- pokemon_dont_go.py
+++ pokemon_dont_go.py
@@ -1,3 +1,10 @@
+def increase_value(list_, remove):
+    list_ = [idx + remove for idx in list_ if idx <= remove]
+    return list_
+
+def decrease_value(list_, remove):
+    list_ = [idx - remove for idx in list_ if idx > remove]
+    return list_
 
 sequence_of_distances = list(map(int, input().split()))
 removed_indices = []
@@ -8,8 +15,9 @@ while sequence_of_distances:
     if index < 0:
         removed_indices = sequence_of_distances.pop(0)
         sequence_of_distances.insert(0, sequence_of_distances[-1])
+        sequence_of_distances = [idx + removed_indices for idx in sequence_of_distances]
 
     elif index > len(sequence_of_distances) + 1 :
         removed_indices = sequence_of_distances.pop(-1)
         sequence_of_distances.append(sequence_of_distances[0])
-        
+
