77f3ac12a4b77a954e65f028a124edf224bd7bfb
Georgi Angelov <gtangelov76@gmail.com>
2026-02-06 14:49:36 +0200

    Update electron_distribution.py

--- electron_distribution.py
+++ electron_distribution.py
@@ -1,2 +1,18 @@
 number_of_electrons = int(input())
 
+shell = []
+position = 0
+
+while number_of_electrons > 0:
+    position += 1
+    current_shell = 2 * position ** 2
+
+    if number_of_electrons < current_shell:
+        shell.append(number_of_electrons)
+        break
+
+    shell.append(current_shell)
+    number_of_electrons -= current_shell
+
+print(shell)
+
