82fbc24151f64e4f603a0e53351debfc522cffb3
Georgi Angelov <gtangelov76@gmail.com>
2026-02-07 01:19:53 +0200

    Update pokemon_dont_go.py

--- pokemon_dont_go.py
+++ pokemon_dont_go.py
@@ -13,11 +13,14 @@ while sequence_of_distances:
     index = int(input())
 
     if index < 0:
-        removed_indices = sequence_of_distances.pop(0)
+        current_removed = sequence_of_distances.pop(0)
         sequence_of_distances.insert(0, sequence_of_distances[-1])
-        sequence_of_distances = [idx + removed_indices for idx in sequence_of_distances]
+        removed_indices.append(current_removed)
 
     elif index > len(sequence_of_distances) + 1 :
-        removed_indices = sequence_of_distances.pop(-1)
+        current_removed = sequence_of_distances.pop(-1)
         sequence_of_distances.append(sequence_of_distances[0])
+        removed_indices.append(current_removed)
+
+        
 
