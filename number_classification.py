a872d0632ed85de6e46a169bd3186cafd28cf5c7
Georgi Angelov <gtangelov76@gmail.com>
2026-02-06 13:55:59 +0200

    Update number_classification.py

--- number_classification.py
+++ number_classification.py
@@ -5,4 +5,7 @@ negative_lst = [num for num in numbers if int(num) < 0]
 even_lst = [num for num in numbers if int(num) % 2 == 0]
 odd_lst = [num for num in numbers if int(num) % 2 != 0]
 
-print(f"")
+print(f"Positive: {', '.join(positive_lst)}")
+print(f"Negative: {', '.join(negative_lst)}")
+print(f"Even: {', '.join(even_lst)}")
+print(f"Odd: {', '.join(odd_lst)}")
