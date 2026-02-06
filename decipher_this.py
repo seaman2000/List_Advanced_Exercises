d60c61de1b2e168f5adbe15f43bb16ac656e0b12
Georgi Angelov <gtangelov76@gmail.com>
2026-02-06 22:54:10 +0200

    Update decipher_this.py

--- decipher_this.py
+++ decipher_this.py
@@ -1,3 +1,11 @@
 decipher_message = input().split()
+
 for message in decipher_message:
-    
+    char_codes = [digit_or_char for digit_or_char in message if digit_or_char.isdigit()]
+    char_code = "".join(char_codes)
+    text = [digit_or_char for digit_or_char in message if digit_or_char.isalpha()]
+
+    text[0], text[-1] = text[-1], text[0]
+    text.insert(0, chr(int(char_code)))
+
+    print("".join(text), end= " ")
