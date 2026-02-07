f78259a18a15ea4eb624a07a0e7d7cada006fcb5
Georgi Angelov <gtangelov76@gmail.com>
2026-02-07 16:32:48 +0200

    Update softuni_course_planning.py

--- softuni_course_planning.py
+++ softuni_course_planning.py
@@ -1,6 +1,20 @@
-def add():
-    pass
-def 
+def add(lesson, list_of_lessons):
+    if lesson not in list_of_lessons:
+        list_of_lessons.append(lesson)
+
+
+def insert(lesson, index_, list_of_lessons):
+    if lesson not in list_of_lessons:
+        list_of_lessons.insert(index_, lesson)
+
+
+def remove(lesson, list_of_lessons):
+    if lesson in list_of_lessons:
+        list_of_lessons.remove(lesson)
+
+
+def swap(first, second, list_of_lessons):
+
 
 
 
@@ -11,3 +25,22 @@ while True:
     command = input()
     if command == "course start":
         break
+    parts = command.split(":")
+    current_command = parts[0]
+    lesson_title = parts[1]
+
+    if current_command == "Add":
+        add(lesson_title, lessons)
+
+    elif current_command == "Insert":
+        index = parts[2]
+        insert(lesson_title, index, lessons)
+
+    elif current_command == "Remove":
+        remove(lesson_title, lessons)
+
+    elif current_command == "Swap":
+        first_lesson = parts[1]
+        second_lesson = parts[2]
+
+
