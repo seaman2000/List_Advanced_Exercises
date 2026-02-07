86492e58c98e357bc7496a49964dd2ab7e73807f
Georgi Angelov <gtangelov76@gmail.com>
2026-02-07 02:06:21 +0200

    Update which_are_in.py

--- which_are_in.py
+++ which_are_in.py
@@ -4,6 +4,7 @@ second_sequence = input().split(", ")
 my_lst = []
 
 for string in first_sequence:
+    
     for string_ in second_sequence:
         if string in string_ and string not in my_lst:
             my_lst.append(string)
